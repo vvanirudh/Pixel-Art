8890505a69d3db3301f0d6c8c28e88e9ae2423f7
aeroscout <aeroscout@aeroscout-ThinkPad-L430.(none)>
2014-05-23 14:14:36 -0400

    Final submission

---
+++ input.py
@@ -0,0 +1,12 @@
+import png # Using pypng module
+
+def read_png(filename):
+	width, height, pixels, metadata = png.Reader(filename = filename).asRGB8() # Get the pixel data as RGB with 8 bits each
+	pixel_data = []
+
+	for pixel_row in pixels: # For each row in the image
+		pixel = []
+		while pixel_row:
+			pixel.append((pixel_row.pop(0),pixel_row.pop(0),pixel_row.pop(0))) # Get the RGB value of the pixel
+		pixel_data.append(pixel)
+	return pixel_data
